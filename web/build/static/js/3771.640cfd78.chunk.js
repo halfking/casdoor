633ad4144effc8a0f201a3b029acc451071a9f46
huangzhouzixuan <56551681@qq.com>
2026-04-01 09:39:26 +0800

    add ignore files

---
+++ web/build/static/js/3771.640cfd78.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[3771],{23771:function(e,n,t){t.r(n),t.d(n,{WebSocket:function(){return o}});var o=function(){if("undefined"!==typeof WebSocket)return WebSocket;if("undefined"!==typeof global.WebSocket)return global.WebSocket;if("undefined"!==typeof window.WebSocket)return window.WebSocket;if("undefined"!==typeof self.WebSocket)return self.WebSocket;throw new Error("`WebSocket` is not supported in this environment")}()}}]);
+//# sourceMappingURL=3771.640cfd78.chunk.js.map
